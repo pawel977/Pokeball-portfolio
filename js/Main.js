2dc2032030ca90626fc67ba1def31ddf13acf73d
Paweł Sawicki <psawicki97@gmail.com>
2018-07-05 14:37:02 +0200

    ADD | add butons to pokeball

--- js/Main.js
+++ js/Main.js
@@ -1,10 +1,41 @@
 const
     timeRotate = `2s`,
     TimeRotateMs = 2000,
-    click = document.querySelector('#click'),
     upper = document.querySelector('#upper'),
-    pokeball = document.querySelector('#pokeball')
-    divMe = `<div id= "me"></div>`;
+    click = document.querySelector('#click'),
+    lower = document.querySelector('#lower'),
+    pokeball = document.querySelector('#pokeball'),
+    startButtons = document.querySelectorAll('.buttonsStart'),
+
+
+
+    divMe = document.createElement("div"),
+    divAbout = document.createElement("div"),
+    divPortfolio = document.createElement("div"),
+    divCv = document.createElement("div");
+
+
+divMe.setAttribute('id', 'zdj');
+
+divAbout.setAttribute('id', 'about');
+divPortfolio.setAttribute('id', 'portfolio');
+divCv.setAttribute('id', 'Cv');
+
+divAbout.setAttribute("class", "buttonsStart");
+divPortfolio.setAttribute("class", "buttonsStart");
+divCv.setAttribute("class", "buttonsStart");
+
+getDivAbout = document.createElement("div");
+getDivPortfolio = document.createElement("div");
+getDivCv = document.createElement("div");
+getDivMe = document.querySelector("#zdj");
+
+
+upper.appendChild(divMe);
+
+lower.appendChild(divAbout);
+lower.appendChild(divPortfolio);
+lower.appendChild(divCv);
 
 click.addEventListener("click", () => {
         upper.style.background = 'Gold';
@@ -24,8 +55,14 @@ let AfterClikOnPokeball = function () {
             pokeball.style.transform = `scale(1.5)`;
             click.style.opacity = 0.0;
             click.style.transition = timeRotate;
+            startButtons.style.display = 'flex';
+            startButtons.style.opacity = '1';
+
+
         }, TimeRotateMs / 3
-    )
+    );
+    click.style.display = "hidden";
+
 
 
 };
