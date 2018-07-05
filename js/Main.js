c87d92269becbbdd78cf9fdbf478c2c9b45209a6
Paweł Sawicki <psawicki97@gmail.com>
2018-07-05 20:38:05 +0200

    ADD | Page with game

--- js/Main.js
+++ js/Main.js
@@ -13,6 +13,8 @@ const
     butPortfolio = document.createElement("button"),
     butCv = document.createElement("button");
 
+butRefresh = document.createElement('button');
+
 
 divMe.setAttribute('id', 'divMe');
 
@@ -23,6 +25,8 @@ butCv.setAttribute('id', 'cv');
 butAbout.setAttribute("class", "buttonsStart");
 butPortfolio.setAttribute("class", "buttonsStart");
 butCv.setAttribute("class", "buttonsStart");
+
+butRefresh.setAttribute('id', 'back');
 
 
 
@@ -61,14 +65,14 @@ let AfterClikOnPokeball = function () {
         divMe.style.opacity = `0.1`;
         divMe.style.backgroundcolor = "red";
 
-        setTimeout(()=>{
-            divMe.style.transition = ` 2s`;
+        setTimeout(() => {
+            divMe.style.transition = `2s`;
             divMe.style.opacity = `1`;
-            
-            setTimeout(()=>{
-              //  divMe.style.backgroundcolor = "blank";
-            },1000);
-        },500);
+
+            setTimeout(() => {
+                //  divMe.style.backgroundcolor = "blank";
+            }, 1000);
+        }, 500);
     }, 1000);
 
     setTimeout(() => {
@@ -77,20 +81,20 @@ let AfterClikOnPokeball = function () {
         butAbout.style.display = 'inline-block';
         butAbout.style.opacity = `0.9`;
         butAbout.style.transition = `2s`;
-    }, 2500);
+    }, 2000);
 
     setTimeout(() => {
         butPortfolio.style.display = 'inline-block';
         butPortfolio.style.opacity = `0.9`;
         butPortfolio.style.transition = `2s`;
-    }, 3000);
+    }, 2200);
 
     setTimeout(() => {
         butCv.style.display = 'inline-block';
         butCv.style.opacity = `0.9`;
         butCv.style.transition = `2s`;
         click.style.display = "none";
-    }, 3500);
+    }, 2800);
 
 
     click.style.opacity = "0.0";
@@ -114,19 +118,279 @@ let AfterClikOnPokeball = function () {
             pokeball.style.transition = timeRotate;
 
             setTimeout(() => {
+
+                setTimeout(() => {
+                    pokeball.style.display = 'none';
+                }, 1000);
+
                 headPageAbout.style.display = `block`;
                 pageAbout.style.display = `block`;
 
                 headPageAbout.style.opacity = "0.9";
                 headPageAbout.style.transition = timeRotate;
 
+                pageAbout.style.opacity = "0.95";
+                pageAbout.style.transition = timeRotate;
+                pageAbout.innerHTML = `
+                
+                
+                Witam.</br>
+                <tab>Jestem młodym, studiującym <b>Junior Front-End Developerem</b>.</br>Aktualnie studiuję Informatykę na <b>Polsko-Japańskiej Akademii Technik komputerowych w Warszawie.</b></br>
+                Moja przygoda z progromwaniem zaczeła się pare lat temu kiedy zapisałem się do <b>Liceum PJATK w klasie o profilu informatycznym</b> gdzie zaczynałem swoją przygodę z językami backendowi.
+                Jednak swoja prawdziwą miłośc poznałem 2 lata temu, jest to <b>Front-End.</b></br></br>
+                
+                Rozwijam się w każdej chwili w tym kierunku, Moje umiejetności to:</br>
+                -js(5/6)</br>
+                -HTML 5</br>
+                -css 3</br>
+                -sass</br>
+                -Angular</br>
+                -oraz wiele innnych.</br>
+                </br>
+                Gorąco zapraszam do kontaktu na mail: psawicki97@gmail.com</br>
+                Pozdrawiam</br>Paweł Sawicki.`;
+
+                headPageAbout.appendChild(butRefresh);
+
+                butRefresh.addEventListener('click', () => {
+                    location.reload();
+                });
 
 
 
             }, 1000);
 
         }, 5000);
+        ///////////////////////game////////////////
+        butPortfolio.addEventListener('click', () => {
 
-    });
+
+
+            pokeball.style.opacity = '0';
+            pokeball.style.transition = timeRotate;
+
+
+
+            setTimeout(() => {
+                const pageGame = document.createElement(`div`);
+                pageGame.setAttribute('id', 'pageGame');
+                document.body.appendChild(pageGame);
+                const allCanv = document.createElement('canvas');
+                allCanv.setAttribute('id', 'allCanv');
+                pageGame.appendChild(allCanv);
+                
+
+                pageGame.appendChild(butRefresh);
+                const ctx = allCanv.getContext(`2d`);
+
+                allCanv.width = 1000;
+                allCanv.height = 500;
+
+                const cw = allCanv.width;
+                const ch = allCanv.height;
+
+                const ballSize = 10;
+
+                let ballX = (cw / 2) - ballSize / 2;
+                let ballY = (ch / 2) - ballSize / 2;
+
+                const paletH = 75;
+                const paletW = 15;
+
+                const playerX = 15;
+                const aiX = cw - (playerX + paletW / 2);
+
+                const lineW = 4;
+                const lineH = 5;
+
+                let ballSpeedX = 0.5;
+                let ballSpeedY = 0.5;
+
+                let playerY = ch / 2 - (paletH / 2);
+                let aiY = ch / 2 - (paletH / 2);
+
+
+
+                let player = function () {
+                    ctx.fillStyle = `black`;
+                    ctx.fillRect(playerX, playerY, paletW, paletH);
+                }
+                let ai = function () {
+                    ctx.fillStyle = `black`;
+                    ctx.fillRect(aiX, aiY, paletW, paletH);
+                }
+
+                const ball = function () {
+                    ctx.fillStyle = `white`;
+                    ctx.fillRect(ballX, ballY, ballSize, ballSize);
+
+                    ballX += ballSpeedX;
+                    ballY += ballSpeedY;
+                    if (ballY <= 0 || ballY + ballSize / 2 >= ch) {
+                        ballSpeedY = -ballSpeedY;
+                        speedUp();
+                    }
+                    if (ballX <= 0 || ballX + ballSize / 2 >= cw) {
+                        ballSpeedX = -ballSpeedX;
+                        speedUp();
+                    }
+                }
+
+
+
+                function table() {
+                    ctx.fillStyle = `green`;
+                    ctx.fillRect(0, 0, cw, ch);
+                    //linie srodka
+                    for (let i = 0; i < ch; i += 10) {
+                        ctx.fillStyle = `grey`;
+                        ctx.fillRect(cw / 2 - lineW / 2, i, lineW, lineH)
+
+                    }
+                }
+                topCanvas = allCanv.offsetTop;
+
+                function playerPosition(e) {
+                    playerY = e.clientY - topCanvas - paletH / 2;
+
+                    if (playerY >= ch - paletH) {
+                        playerY = ch - paletH;
+                    }
+                    if (playerY <= 0) {
+                        playerY = 0;
+                    }
+                };
+
+                /*---------------przyśpieszeniePiłki---------*/
+                function speedUp() {
+                    if (ballSpeedX > 0 && ballSpeedX < 15) {
+                        ballSpeedX += 0.2;
+                    } else if (ballSpeedX < 0 && ballSpeedX > -15) {
+                        ballSpeedX -= 0.2
+                    }
+
+                    if (ballSpeedY > 0 && ballSpeedY < 15) {
+                        ballSpeedY += 0.1;
+                    } else if (ballSpeedY < 0 && ballSpeedY > -15) {
+                        ballSpeedY -= 0.1;
+                    } else {
+                        ballSpeedY = 10;
+                    }
+                    console.log(ballSpeedX + ", " + ballSpeedY);
+                }
+
+                //sterowanie bota
+                function botPos() {
+                    let middlePalet = aiY + paletH / 2;
+                    let middleBall = ballY + ballSize / 2;
+
+                    if (ballX > cw / 2) {
+                        if (middlePalet - middleBall > paletH / 2) {
+                            aiY -= 20;
+
+                        } else if (middlePalet - middleBall > paletH / 4) {
+                            aiY -= 10;
+                        } else if (middlePalet - middleBall < -paletH / 2) {
+                            aiY += 20;
+                        } else if (middlePalet - middleBall < -paletH / 4) {
+                            aiY += 10;
+                        }
+                    }
+
+                    if (ballX <= cw / 2 && ballX > playerX) {
+                        if (middlePalet - middleBall > playerX) {
+                            aiY -= 6;
+                        }
+                        if (middlePalet - middleBall < -playerX) {
+                            aiY += 6;
+                        }
+                    }
+                    if (aiY <= 0) {
+                        aiY = 0;
+                    } else if (aiY + paletH >= ch) {
+                        aiY = ch - paletH;
+                    }
+                }
+
+                allCanv.addEventListener("mousemove", playerPosition)
+
+                // odbicie pilki
+
+
+
+                function odbicie() {
+                    const paletPozSzer = playerX + paletW;
+                    const paletPozWysDol = playerY + paletH;
+                    const trzeciak = (playerY + paletH) / 3;
+
+                    if (((paletPozSzer) >= ballX) &&
+                        (playerY <= (ballY + ballSize / 2)) &&
+                        ((paletPozWysDol) >= ballY)) {
+                        ballSpeedX = -ballSpeedX;
+
+
+
+                        if (playerY <= (ballY - ballSize / 2)) {
+                            ballSpeedY = Math.abs(ballSpeedY) + 1 * Math.random(.5, 2.1);
+                        } else if (ballY > trzeciak && ballY < trzeciak * 2) {
+                            ballSpeedY = 0;
+                        } else if (ballY >= trzeciak * 2 && ballY <= trzeciak * 3) {
+                            if (Math.abs(ballSpeedY) != -1) {
+                                ballSpeedY = -Math.abs(ballSpeedY) + 1;
+                            }
+                        }
+                        speedUp();
+                    }
+
+                    OdbicieAi();
+                }
+
+                function OdbicieAi() {
+                    if (ballX + ballSize >= aiX &&
+                        ballY >= aiY &&
+                        ballY <= aiY + paletH) {
+                        ballSpeedX = -ballSpeedX;
+                        speedUp();
+
+                    }
+                }
+
+                function game() {
+                    table();
+                    player();
+                    botPos();
+                    ai();
+                    ball();
+                    odbicie();
+
+                }
+                setInterval(game, 1000 / 60);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }, 2300);
+
+            butRefresh.addEventListener('click', () => {
+                location.reload();
+            });
+        });
+
+
+    }, 1000);
+
+
+
 
 };
